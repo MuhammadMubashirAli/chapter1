b5139311e5c11a293a523310adae921fb8f1d0f3
MuhammadMubashirAli <121821846+MuhammadMubashirAli@users.noreply.github.com>
2023-05-06 16:08:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+//1. Write a script to greet your website visitor using JS alert 
+//box.
+alert('Welcome');
+// 2. Write a script to display following message on your web 
+// page:
+
+alert('Error! Please enter a valid password');
+
+// 2. Write a script to display following message on your web 
+// page:
+alert('Welcome to JS land \n happy coding! ');
+
+// 4. Write a script to display following messages in sequence:
+ alert("happy coding"+ "\n" + "Prevent this page from creating additional dialogs")
+
+// 5. Generate the following message through browser’s 
+// developer console:
+ alert("Hello.. i can run js in my web browsers console")
